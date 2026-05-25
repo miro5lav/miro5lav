70ae105d2115323debe78678226502e3e72cf570
Miroslaw R <mirrad@10g.pl>
2026-05-25 08:43:12 +0000

    Reorganize assets and images folder and put them under docs folder.

--- assets/js/respond.js
+++
@@ -1,3 +0,0 @@
-// Placeholder respond.js
-// This file exists so the site can load the referenced script without producing a 404.
-// Add a real polyfill here only if you need to support older browsers.
